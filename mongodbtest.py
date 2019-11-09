0998f1a4ec302d4654eb92730120a19d44008752
Adrian Havenga-Bennett <adrian.havenga@yahoo.com>
2019-11-09 13:13:59 +0000

    code for reference

---
+++ mongodbtest.py
@@ -0,0 +1,28 @@
+import pymongo
+# import os
+
+# MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+# conn = mongo_connect(MONGODB_URI)
+conn = mongo_connect("mongodb+srv://InvAdrian:Iambutts1985@myfirstcluster-e8a5p.mongodb.net/myTestDB?retryWrites=true&w=majority")
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+coll.update_many({"nationality": "english"}, {"$set": {"occupation": "homeless"}})
+
+documents = coll.find({"nationality": "english"})
+
+for doc in documents:
+    print(doc)
